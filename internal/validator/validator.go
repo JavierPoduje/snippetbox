e901ac6fcf24ce40fd14995f668732cdf5fbb18f
javierpoduje <javier.poduje@gmail.com>
2025-07-06 21:40:01 +0200

    ch 10.4: login users

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -8,7 +8,8 @@ import (
 )
 
 type Validator struct {
-	FieldErrors map[string]string
+	NonFieldErrors []string
+	FieldErrors    map[string]string
 }
 
 var EmailRX = regexp.MustCompile(
@@ -17,7 +18,7 @@ var EmailRX = regexp.MustCompile(
 
 // Valid returns `true` if the FieldErrors map doesn't contain any entries.
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
 }
 
 // AddFieldError adds an error message to the FieldErrros map (so long as no entry already exists for the given key).
@@ -29,6 +30,10 @@ func (v *Validator) AddFieldError(key, message string) {
 	if _, exists := v.FieldErrors[key]; !exists {
 		v.FieldErrors[key] = message
 	}
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 // CheckField adds and error message to the FieldErrors map only if a validation check is not 'ok'.
